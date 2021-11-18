491dc4ff582b449fb68a6bd455f5d36f97d4464e
Alex Tinarsky <alex.tinarsky@mail.ru>
2021-11-18 23:14:03 +0300

    Added class Tree, methods Input() and CreateTree().

---
+++ HuffmanCode/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace HuffmanCode
+{
+    class Program
+    {
+        static List<Node> Input()
+        {
+            var Str = "Алексей Докажите, что энтропия монетки принимает" +
+                " наибольшее значение для правильной монетки. Тинарский";
+            var str = new string(Str.Where(c =>
+            (!char.IsPunctuation(c) && !char.IsWhiteSpace(c) && !char.IsNumber(c))).ToArray()).ToLower();
+            List<Node> list_of_nodes = new();
+            HashSet<char> chars = new(str);
+            foreach (char ch in chars)
+            {
+                list_of_nodes.Add(new Node(ch, str.Count(c => c == ch), null, null));
+            }
+            return list_of_nodes;
+        }
+
+        static List<Node> CreateTree(List<Node> list_of_nodes)
+        {
+            if (list_of_nodes.Count == 1)
+                return list_of_nodes;
+
+            Node first_node = list_of_nodes.Min<Node>();
+            list_of_nodes.Remove(first_node);
+
+            Node second_node = list_of_nodes.Min<Node>();
+            list_of_nodes.Remove(second_node);
+
+            list_of_nodes.Add(new(null, first_node.NumberOfChar + second_node.NumberOfChar,
+                first_node, second_node));
+            return CreateTree(list_of_nodes);
+        }
+
+        static void Main(string[] args)
+        {
+            var list_of_nodes = Input();
+            foreach (Node c in list_of_nodes)
+            {
+                Console.WriteLine($"{c.Character}: {c.NumberOfChar}");
+            }
+
+            var tree = CreateTree(Input());
+            foreach (Node c in tree)
+            {
+                Console.WriteLine($"{c.Character}: {c.NumberOfChar}");
+            }
+        }
+    }
+}
---
+++ HuffmanCode/Tree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HuffmanCode
+{
+    class Node: IComparable<Node>
+    {
+        public char? Character { get; }
+        public int NumberOfChar { get; }
+        public Node Left { get; }
+        public Node Right { get; }
+        public Node(char? c, int num_of_char, Node left, Node right)
+        {
+            NumberOfChar = num_of_char;
+            Left = left;
+            Right = right;
+            Character = c;
+        }
+
+        public int CompareTo(Node other)
+        {
+            if (other == null) 
+                throw new NotImplementedException();
+
+            return NumberOfChar.CompareTo(other.NumberOfChar);
+        }
+    }
+}
